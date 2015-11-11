1063518bedb534ec2d167a77ffb4b5850fc1dd0a
Nestor Alcantara <nestor100@gmail.com>
2015-11-10 20:14:29 -0800

    Implement the two states of tooltips: normal and hidden, the last one is shown when hover. Change show animation of options section.

--- mdl-speed-dial.js
+++ mdl-speed-dial.js
@@ -2,21 +2,21 @@ $(document).ready(function() {
 	$('.mdl-speed-dial__main-fab').hover(openFAB);
 	$('.mdl-speed-dial').mouseleave(closeFAB);
 	$('.mdl-speed-dial__option').hover(showFABTooltip, hideFABTooltip);
-	$('.mdl-speed-dial__tooltip').hide();
+	$('.mdl-speed-dial__tooltip--hidden').hide();
 });
 
 function openFAB(e) {
-	$(this).siblings('.mdl-speed-dial__options').show('normal');
+	$(this).siblings('.mdl-speed-dial__options').fadeIn('fast');
 }
 
 function closeFAB(e) {
-	$(this).children('.mdl-speed-dial__options').hide('normal');
+	$(this).children('.mdl-speed-dial__options').fadeOut('fast');
 }
 
 function showFABTooltip(e) {
-	$(this).children('.mdl-speed-dial__tooltip').show();
+	$(this).children('.mdl-speed-dial__tooltip--hidden').show();
 }
 
 function hideFABTooltip(e) {
-	$(this).children('.mdl-speed-dial__tooltip').hide();
+	$(this).children('.mdl-speed-dial__tooltip--hidden').hide();
 }
