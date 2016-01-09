22c9e51d56739548ebc86b49c6f2b272bef73728
Nestor Alcantara <nestor100@gmail.com>
2016-01-08 18:08:05 -0800

    Implement second icon for main FAB. This second icon replaces the first one when it's spinned.

--- mdl-speed-dial.js
+++ mdl-speed-dial.js
@@ -1,28 +1,51 @@
 $(document).ready(function() {
-	$('.mdl-speed-dial__main-fab').hover(openFAB);
+	$('.mdl-speed-dial__main-fab').mouseenter(openFAB);
 	$('.mdl-speed-dial').mouseleave(closeFAB);
 	$('.mdl-speed-dial__option').hover(showFABTooltip, hideFABTooltip);
 	$('.mdl-speed-dial__tooltip--hidden').hide();
 });
 
+function rotateElement(el, deg, duration){
+	$(el).stop().animate({ rotation: deg }, {
+		duration: duration,
+		step: function(now, fx) {
+			$(this).css({ "transform": "rotate(" + now + "deg)" });
+		}
+	});
+}
+
 function openFAB(e) {
 	var $btn = $(this),
+		$speedDialOptions = $btn.siblings('.mdl-speed-dial__options'),
+		icons = $btn.children('i'),
+		$firstIcon = $(icons[0]),
+		$secondIcon = $(icons[1]);
 		rotate = $btn.hasClass('mdl-speed-dial__main-fab--spin');
 
-	$btn.siblings('.mdl-speed-dial__options').fadeIn('fast');
+	if ($speedDialOptions.css('display') == 'none') {
+		$speedDialOptions.fadeIn('fast');
 
-	if (rotate) {
-		$btn.stop().animate({ rotation: 45 }, {
-			duration: 100,
-			step: function(now, fx) {
-				$(this).css({ "transform": "rotate(" + now + "deg)" });
+		if (rotate) {
+			rotateElement($btn, 360, 300);
+
+			if (icons.length == 2) {
+				if ($firstIcon.hasClass('hide')) {
+					$firstIcon.fadeIn('fast').removeClass('hide');
+					$secondIcon.fadeOut('fast').addClass('hide');
+				} else if ($secondIcon.hasClass('hide')) {
+					$firstIcon.fadeOut('fast').addClass('hide');
+					$secondIcon.fadeIn('fast').removeClass('hide');
+				}
 			}
-		});
+		}
 	}
 }
 
 function closeFAB(e) {
 	var $btn = $(this).children('.mdl-speed-dial__main-fab'),
+		icons = $btn.children('i'),
+		$firstIcon = $(icons[0]),
+		$secondIcon = $(icons[1]);
 		rotate = $btn.hasClass('mdl-speed-dial__main-fab--spin');
 
 	$(this).children('.mdl-speed-dial__options').fadeOut('fast');
@@ -34,6 +57,16 @@ function closeFAB(e) {
 				$(this).css({ "transform": "rotate(" + now + "deg)" });
 			}
 		});
+
+		if (icons.length == 2) {
+			if ($firstIcon.hasClass('hide')) {
+				$firstIcon.fadeIn('fast').removeClass('hide');
+				$secondIcon.fadeOut('fast').addClass('hide');
+			} else if ($secondIcon.hasClass('hide')) {
+				$firstIcon.fadeOut('fast').addClass('hide');
+				$secondIcon.fadeIn('fast').removeClass('hide');
+			}
+		}
 	}
 }
 
