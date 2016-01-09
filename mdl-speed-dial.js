90adb1b4467721d74baca28a0d0731cf67588c6f
Nestor Alcantara <nestor100@gmail.com>
2016-01-08 19:20:08 -0800

    Re-implement second icon for main FAB when spin

--- mdl-speed-dial.js
+++ mdl-speed-dial.js
@@ -17,24 +17,26 @@ function rotateElement(el, deg, duration){
 function openFAB(e) {
 	var $btn = $(this),
 		$speedDialOptions = $btn.siblings('.mdl-speed-dial__options'),
+		isSpeedDialOptionsHidden = $speedDialOptions.css('display') == 'none' ? true : false,
 		icons = $btn.children('i'),
 		$firstIcon = $(icons[0]),
-		$secondIcon = $(icons[1]);
+		$secondIcon = $(icons[1]),
+		hiddenClassName = 'mdl-speed-dial_main-fab-icon--hidden',
 		rotate = $btn.hasClass('mdl-speed-dial__main-fab--spin');
 
-	if ($speedDialOptions.css('display') == 'none') {
+	if (isSpeedDialOptionsHidden) {
 		$speedDialOptions.fadeIn('fast');
 
 		if (rotate) {
 			rotateElement($btn, 360, 300);
 
 			if (icons.length == 2) {
-				if ($firstIcon.hasClass('hide')) {
-					$firstIcon.fadeIn('fast').removeClass('hide');
-					$secondIcon.fadeOut('fast').addClass('hide');
-				} else if ($secondIcon.hasClass('hide')) {
-					$firstIcon.fadeOut('fast').addClass('hide');
-					$secondIcon.fadeIn('fast').removeClass('hide');
+				if ($firstIcon.hasClass(hiddenClassName)) {
+					$firstIcon.fadeIn('fast').removeClass(hiddenClassName);
+					$secondIcon.fadeOut('fast').addClass(hiddenClassName);
+				} else if ($secondIcon.hasClass(hiddenClassName)) {
+					$firstIcon.fadeOut('fast').addClass(hiddenClassName);
+					$secondIcon.fadeIn('fast').removeClass(hiddenClassName);
 				}
 			}
 		}
@@ -45,7 +47,8 @@ function closeFAB(e) {
 	var $btn = $(this).children('.mdl-speed-dial__main-fab'),
 		icons = $btn.children('i'),
 		$firstIcon = $(icons[0]),
-		$secondIcon = $(icons[1]);
+		$secondIcon = $(icons[1]),
+		hiddenClassName = 'mdl-speed-dial_main-fab-icon--hidden',
 		rotate = $btn.hasClass('mdl-speed-dial__main-fab--spin');
 
 	$(this).children('.mdl-speed-dial__options').fadeOut('fast');
@@ -59,12 +62,12 @@ function closeFAB(e) {
 		});
 
 		if (icons.length == 2) {
-			if ($firstIcon.hasClass('hide')) {
-				$firstIcon.fadeIn('fast').removeClass('hide');
-				$secondIcon.fadeOut('fast').addClass('hide');
-			} else if ($secondIcon.hasClass('hide')) {
-				$firstIcon.fadeOut('fast').addClass('hide');
-				$secondIcon.fadeIn('fast').removeClass('hide');
+			if ($firstIcon.hasClass(hiddenClassName)) {
+				$firstIcon.fadeIn('fast').removeClass(hiddenClassName);
+				$secondIcon.fadeOut('fast').addClass(hiddenClassName);
+			} else if ($secondIcon.hasClass(hiddenClassName)) {
+				$firstIcon.fadeOut('fast').addClass(hiddenClassName);
+				$secondIcon.fadeIn('fast').removeClass(hiddenClassName);
 			}
 		}
 	}
