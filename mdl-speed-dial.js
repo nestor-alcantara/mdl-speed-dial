7dc97c2931e090744a1b3551261230c57c8c6f1c
Nestor Alcantara <nestor100@gmail.com>
2016-01-08 16:48:17 -0800

    Implement main FAB rotation

--- mdl-speed-dial.js
+++ mdl-speed-dial.js
@@ -6,11 +6,35 @@ $(document).ready(function() {
 });
 
 function openFAB(e) {
-	$(this).siblings('.mdl-speed-dial__options').fadeIn('fast');
+	var $btn = $(this),
+		rotate = $btn.hasClass('mdl-speed-dial__main-fab--spin');
+
+	$btn.siblings('.mdl-speed-dial__options').fadeIn('fast');
+
+	if (rotate) {
+		$btn.stop().animate({ rotation: 45 }, {
+			duration: 100,
+			step: function(now, fx) {
+				$(this).css({ "transform": "rotate(" + now + "deg)" });
+			}
+		});
+	}
 }
 
 function closeFAB(e) {
+	var $btn = $(this).children('.mdl-speed-dial__main-fab'),
+		rotate = $btn.hasClass('mdl-speed-dial__main-fab--spin');
+
 	$(this).children('.mdl-speed-dial__options').fadeOut('fast');
+
+	if (rotate) {
+		$btn.stop().animate({ rotation: 0 }, {
+			duration: 100,
+			step: function(now, fx) {
+				$(this).css({ "transform": "rotate(" + now + "deg)" });
+			}
+		});
+	}
 }
 
 function showFABTooltip(e) {
